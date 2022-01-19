34d04882971bdfa69f7f1821e856fa52ce4d24bd
maxmakk <90equals50@gmail.com>
2022-01-19 17:58:20 +0200

    rewrite all js code from scratch

--- script.js
+++ script.js
@@ -1,230 +1,221 @@
-const display = document.querySelector("span");
-const operand = document.querySelectorAll("[data-num]");
-const operator = document.querySelectorAll("[data-operator]");
-const equalSign = document.querySelector("#equal");
-const minus = document.querySelector("#plusminus");
-const dot = document.querySelector("#dot");
-const bksp = document.querySelector("#backspace");
-const percent = document.querySelector("#perc");
-const allClear = document.querySelector("#ac");
+const btns = document.querySelectorAll('button')
+const display = document.querySelector('.display_inner')
 
-let mainArr = [];
-
-let firstNum = [];
-let secondNum = [];
-let curOp = [];
-
-function add(a, b) {
-  return Math.round((a + b) * 100) / 100;
-}
-
-function subtract(a, b) {
-  return Math.round((a - b) * 100) / 100;
-}
-
-function multiply(a, b) {
-  return Math.round(a * b * 100) / 100;
-}
-
-function divide(a, b) {
-  return Math.round((a / b) * 100) / 100;
-}
-
-function operate(operator, num1, num2) {
-  return operator(num1, num2);
-}
+const leftNum = []
+const operator = []
+const rightNum = []
 
 function updateDisplay() {
-  let replaceOp = curOp[0];
-  let newOp = "";
-  if (replaceOp === "mul") {
-    newOp = " × ";
-  } else if (replaceOp === "div") {
-    newOp = " ÷ ";
-  } else if (replaceOp === "sub") {
-    newOp = " - ";
-  } else if (replaceOp === "add") {
-    newOp = " + ";
-  }
-  let arr = [...firstNum, newOp, ...secondNum];
-  display.textContent = arr.join("").slice(-9);
-}
-
-allClear.addEventListener("click", () => {
-  firstNum = [];
-  secondNum = [];
-  curOp = [];
-  updateDisplay();
-});
-
-percent.addEventListener("click", calcPerc);
-
-function calcPerc() {
-  if (secondNum.length === 0) {
-    let a = firstNum.join("");
-    let b = +a / 100;
-    firstNum = String(b).split("");
-  } else if (curOp.length === 1 && !secondNum.includes("%")) {
-    secondNum.push("%");
-  }
-  updateDisplay();
-}
-
-dot.addEventListener("click", addDot);
-
-function addDot() {
-  if (secondNum.length === 0 && curOp.length === 0) {
-    if (!firstNum.includes(".")) {
-      firstNum.push(".");
+  const [...arr] = [...leftNum,...operator,...rightNum]
+  const replaceStr = [].concat(...arr).map(el => {
+    if(el === '/') {
+      return el = '÷'
+    } else if (el === '*') {
+      return el = '×'
+    } else if (el === '-' || el === '+' || el === '.' || el === '%') {
+      return el
+    } else if (isFinite(el)) {
+      return el
     }
-  } else if (curOp.length === 1) {
-    if (!secondNum.includes(".")) {
-      secondNum.push(".");
-    }
-  }
-  updateDisplay();
+  })
+  display.textContent = replaceStr.join('').slice(-8)
 }
 
-bksp.addEventListener("click", deleteChar);
+btns.forEach(el => el.addEventListener('click', function() {
+  addChar(el.id)
+  updateDisplay()
+}))
 
-function deleteChar() {
-  if (firstNum.length >= 1 && curOp.length === 0) {
-    firstNum.pop();
-  } else if (curOp.length === 1 && secondNum.length === 0) {
-    curOp.pop();
-  } else if (secondNum.length >= 1) {
-    secondNum.pop();
-  }
-  updateDisplay();
-}
-
-minus.addEventListener("click", function () {
-  if (secondNum.length === 0 && curOp.length === 0) {
-    if (!firstNum.includes("-")) {
-      firstNum.unshift("-");
-    } else {
-      firstNum.shift();
-    }
-  } else if (curOp.length === 1) {
-    if (!secondNum.includes("-")) {
-      secondNum.unshift("-");
-    } else {
-      secondNum.shift();
-    }
-  }
-  updateDisplay();
-});
-
-operator.forEach((el) => {
-  el.addEventListener("click", function () {
-    addOperator(el.id);
-  });
-});
-
-function addOperator(e) {
-  if (firstNum.length === 0) {
-    return;
-  } else if (secondNum.length === 0 && curOp.length === 1) {
-    curOp.pop();
-  }
-  if (curOp.length === 1) {
-    calculate();
-  }
-  curOp.push(e);
-  updateDisplay();
-}
-
-operand.forEach((el) => {
-  el.addEventListener("click", function () {
-    addOperand(el.id);
-  });
-});
-
-function addOperand(e) {
-  if (curOp.length === 1) {
-    secondNum.push(e);
-  } else if (curOp.length === 0) {
-    firstNum.push(e);
-  }
-  updateDisplay();
-}
-
-equalSign.addEventListener("click", calculate);
-
-// *************************************************** Calculate
-
-function calculate() {
-  if (
-    secondNum.length === 0 ||
-    curOp.length === 0 ||
-    (firstNum[0] === "." && firstNum.length === 1) ||
-    (secondNum[0] === "." && secondNum.length === 1) ||
-    (firstNum[0] === "-" && firstNum.length === 1) ||
-    (secondNum[0] === "-" && secondNum.length === 1)
-  ) {
-    return;
-  }
-  if (secondNum.includes("%")) {
-    let a = secondNum.slice(0, -1).join("");
-    let b = firstNum.join("");
-    let c = Math.round((+b / 100) * +a * 100) / 100;
-    secondNum = String(c).split("");
-  }
-  let a = firstNum.join("");
-  let b = secondNum.join("");
-
-  let res;
-  switch (curOp[0]) {
-    case "div":
-      if (b === "0") {
-        res = "hi";
-      } else {
-        res = operate(divide, +a, +b);
+function addChar(id) {
+  if(isNumberic(id)) {
+    if(operator.length === 0) {
+      if(leftNum[leftNum.length - 1] === '%') {
+        return
       }
-      break;
-    case "mul":
-      res = operate(multiply, +a, +b);
-      break;
-    case "add":
-      res = operate(add, +a, +b);
-      break;
-    case "sub":
-      res = operate(subtract, +a, +b);
-      break;
+      leftNum.push(id)
+    } else {
+      if(rightNum[rightNum.length - 1] === '%') {
+        return
+      }
+      rightNum.push(id)
+    }
+  } else if(isOperator(id)) {
+    if(operator.length && isNumberic(rightNum.join(''))) {
+      operate(id)
+    } else if(isNumberic(leftNum.join('')) && operator.length === 0) {
+      operator.push(id)
+    } else if (operator.length === 1 && rightNum.length === 0) {
+      operator.length = 0
+      operator.push(id)
+    }
+  } else if((id === '=' || id === 'Enter') && isNumberic(rightNum.join(''))) {
+    operate()
+  } else if(id === '.') {
+    if(operator.length === 0) {
+      if(leftNum[leftNum.length - 1] === '.') {
+        leftNum.pop()
+      } else if(!leftNum.includes('.')) {
+        if(leftNum[leftNum.length - 1] === '%') {
+          return 
+        }
+        leftNum.push('.')
+      }
+    } else {
+      if(rightNum[rightNum.length - 1] === '.') {
+        rightNum.pop()
+      } else if(!rightNum.includes('.')) {
+        if(rightNum[rightNum.length - 1] === '%') {
+          return 
+        }
+        rightNum.push('.')
+      }
+    }
+  } else if(id === '+/-') {
+    if(operator.length === 0) {
+      if(leftNum.includes('-')) {
+        leftNum.shift()
+      } else {
+        leftNum.unshift('-')
+      }
+    } else if(operator.length) {
+      if(rightNum.includes('-')) {
+        rightNum.shift()
+      } else {
+        rightNum.unshift('-')
+      }
+    }
+  } else if(id === '%') {
+    if(operator.length === 0) {
+      if(leftNum.includes('%')) {
+        leftNum.pop()
+      } else {
+        if(leftNum[leftNum.length - 1] === '.' ||
+           !isNumberic(leftNum.join(''))) {
+          return
+        }
+        leftNum.push('%')
+      }
+    } else if(operator.length) {
+      if(rightNum.includes('%')) {
+        rightNum.pop()
+      } else {
+        if(rightNum[rightNum.length - 1] === '.' ||
+           !isNumberic(rightNum.join(''))) {
+          return
+        }
+        rightNum.push('%')
+      }
+    }
+  } else if(id === 'Backspace') {
+    if(rightNum.length) {
+      rightNum.pop()
+    } else if(operator.length) {
+      operator.pop()
+    } else {
+      leftNum.pop()
+    }
+  } else if(id === 'ac') {
+    leftNum.length = 0
+    rightNum.length = 0
+    operator.length = 0
   }
-  curOp.shift();
-  firstNum = [];
-  secondNum = [];
-  firstNum = String(res).split("");
-  updateDisplay();
 }
 
-// ********************************************** Keyboard
-
-window.addEventListener("keydown", function (e) {
-  console.log(e);
-  operand.forEach((el) => {
-    if (el.id === e.key) {
-      addOperand(e.key);
-    }
-  });
-  if (e.key === "-") {
-    addOperator("sub");
-  } else if (e.key === "+") {
-    addOperator("add");
-  } else if (e.key === "/") {
-    addOperator("div");
-  } else if (e.key === "*") {
-    addOperator("mul");
-  } else if (e.key === ".") {
-    addDot();
-  } else if (e.key === "Enter" || e.key === "=") {
-    calculate();
-  } else if (e.key === "Backspace") {
-    deleteChar();
-  } else if (e.key === "%") {
-    calcPerc();
-  } else {
-    return;
+function checkForPairs() {
+  if(arr.length === 3) {
+    operate()
   }
-});
+}
+
+function operate(o) {
+  const nextOperator = o
+  if(leftNum.includes('%') || rightNum.includes('%')) {
+    getPerc()
+  }
+  const first = parseFloat(leftNum.join(''))
+  const second = parseFloat(rightNum.join(''))
+  let result = ''
+  switch(operator.join('')) {
+    case '*':
+      result = String(multiply(first,second))
+      break
+    case '+':
+      result = String(add(first,second))
+      break
+    case '-':
+      result = String(subtract(first,second))
+      break
+    case '/':
+      if(second === 0) {
+        alert('¯\\_(ツ)_/¯')
+        return
+      }
+      result = String(divide(first,second))
+      break
+  }
+  allClear()
+  result.split('').forEach(el => leftNum.push(el))
+  if(nextOperator) {
+    operator.push(o)
+  }
+  updateDisplay()
+}
+
+function allClear() {
+  leftNum.length = 0
+  rightNum.length = 0
+  operator.length = 0
+}
+
+function add (a, b) {
+  return a + b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide (a, b) {
+  return a / b
+}
+
+function subtract (a, b) {
+  return a - b
+}
+
+function isNumberic(str) {
+  let n = str
+  if(str.includes('%')) {
+    n = str.slice(0,-1)
+  }
+  return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+function isOperator(o) {
+  if(o === '/' || o === '*' || o === '+' || o === '-') {
+    return true
+  } else {
+    return false
+  }
+}
+
+function getPerc() {
+  if(leftNum.includes('%')) {
+    const perc = parseFloat(leftNum.join('').slice(0,-1))
+    const num = parseFloat(rightNum.join(''))
+    let res = String((perc * num) / 100)
+    leftNum.length = 0
+    res.split('').forEach(el => leftNum.push(el))
+  } else if(rightNum.includes('%')) {
+    const perc = parseFloat(rightNum.join('').slice(0,-1))
+    const num = parseFloat(leftNum.join(''))
+    let res = String((perc * num) / 100)
+    rightNum.length = 0
+    res.split('').forEach(el => rightNum.push(el))
+  }
+}
+
+window.addEventListener('keydown', function(e) {
+  addChar(e.key)
+  updateDisplay()
+})
